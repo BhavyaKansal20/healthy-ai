dbb2485ffd0e1311c420a579d38fe395201c82b5
Bhavya Kansal <kansalbhavya27@gmail.com>
2026-04-08 10:47:01 +0530

    Remove ToTensor numpy dependency in brain transform pipeline

--- app.py
+++ app.py
@@ -191,7 +191,8 @@ def _ensure_brain_runtime_loaded():
 
         brain_transform = transforms.Compose([
             transforms.Resize((224, 224)),
-            transforms.ToTensor(),
+            transforms.PILToTensor(),
+            transforms.ConvertImageDtype(torch.float32),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
